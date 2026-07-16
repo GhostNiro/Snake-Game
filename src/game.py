39f96eadd2b26eed6266f7b7a9dde45b17e08574
Niro <chergouniro@gmail.com>
2026-07-16 02:52:46 +0200

    [US-003]feat : Affichage du snake.

--- src/game.py
+++ src/game.py
@@ -1,12 +1,15 @@
 import pygame
 import sys
-from src.constants import LARGEUR , LONGUEUR
+from src.constants import  LONGUEUR, BACKGROUND_COLOR
+from src.snake import Snake
+
 class Game : 
     def __init__(self):
         pygame.init()
-        pygame.display.set_mode((LONGUEUR, LARGEUR))
+        self.screen = pygame.display.set_mode((LONGUEUR, LONGUEUR))
         pygame.display.set_caption("SNAKE GAME")  
         self.en_cours = True #variable qui permet de savoir si le jeu est en cours ou non (pour la boucle principal du jeu)
+        self.snake = Snake(10, 10)
     
     def handle_events(self):
         for event in pygame.event.get():
@@ -17,6 +20,9 @@ class Game :
     def run(self) :
         while self.en_cours:
             self.handle_events()
+            self.screen.fill(BACKGROUND_COLOR)
+            self.snake.draw(self.screen)
+            pygame.display.flip()
             
         pygame.quit()
         sys.exit()
